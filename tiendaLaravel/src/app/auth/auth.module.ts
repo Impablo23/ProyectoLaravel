31d251e18234f43169ef75479c86e4ac18be63ad
Pablo Rico Machío <150745993+Impablo23@users.noreply.github.com>
2025-02-17 19:30:23 +0100

    update

--- tiendaLaravel/src/app/auth/auth.module.ts
+++ tiendaLaravel/src/app/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutAuthComponent } from './layout-auth/layout-auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
--- tiendaLaravel/src/app/auth/register/register.component.ts
+++ tiendaLaravel/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthServiceService } from 'src/services/auth/auth-service.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  public nombre_user: string = '';
+  public email: string = '';
+  public password: string = '';
+
+  constructor(
+    private authService: AuthServiceService,
+    private toast: MatSnackBar
+  ){}
+
+  ngOnInit() {
+    this.nombre_user = '';
+    this.email = '';
+    this.password = '';
+  }
+
+  public registrarUsuario() {
+    if (this.nombre_user.length < 5){
+      this.toast.open('El nombre de usuario debe tener al menos 5 caracteres', 'Dabuten', {
+        duration: 3000
+      });
+      return;
+    }
+
+    if(!this.email.includes('@')){
+      this.toast.open('El email no es válido', 'Dabuten', {
+        duration: 3000
+      });
+      return;
+    }
+
+    if (this.password.length < 8){
+      this.toast.open('La contraseña debe tener al menos 8 caracteres', 'Dabuten', {
+        duration: 3000
+      });
+      return;
+    }
+
+    
+
+    
+  }
+
+
 
 }
